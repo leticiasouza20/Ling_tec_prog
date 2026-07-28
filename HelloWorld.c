d1526ff2bf878f5e25f5b98f6eb497e7f1e0bf62
Leticia Souza <souzaleticiamga@gmail.com>
2026-07-28 09:12:04 -0300

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,5 @@
+#incluide <stdio.h> //O token é o #, depois ele pesquisa, <biblioteca que já pré compilado>, linguagem presudural (uma instrução atrás da outra)
+int main (){  
+  printf ("");              
+  return 0;  
+}
