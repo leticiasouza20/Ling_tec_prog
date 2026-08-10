ee8fab482af6e150fee0adce9c561065b7d02ea5
Leticia Souza <souzaleticiamga@gmail.com>
2026-08-10 08:09:34 -0300

    Create areadotriangulo.c

---
+++ areadotriangulo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+ int base, altura;
+ float area;
+	
+ base = 10;
+ altura = 20;
+	
+ area = (base*altura)/2;
+	
+ printf("A area do Triangulo Retangulo de Base = %d e altura = %d e %.1f", base, altura, area);
+	
+ return 0;
+}
