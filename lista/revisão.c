0622d98e7d116c16acf79b8825c243815bc93288
Leticia Souza <souzaleticiamga@gmail.com>
2026-09-13 18:28:24 -0300

    Update revisão.c

--- lista/revisão.c
+++ lista/revisão.c
@@ -117,7 +117,238 @@ int main(int argc, char *argv[]) {
         printf("REPROVADO(A)!");
     }
 
+// EXERCÍCIO 4
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int valor,n100, n50, n10, n5, n2, n1;
+
+    printf("Digite o valor do saque: R$ ");
+    scanf("%d", &valor);
+
+    n100 = valor / 100;
+    valor = valor % 100;
+
+    n50 = valor / 50;
+    valor = valor % 50;
+
+    n10 = valor / 10;
+    valor = valor % 10;
+
+    n5 = valor / 5;
+    valor = valor % 5;
+
+    n2 = valor / 2;
+    valor = valor % 2;
+
+    n1 = valor / 1;
+    valor = valor % 1;
+
+    printf("\n RESUMO DO SAQUE \n");
+    printf("Notas de R$ 100: %d\n", n100);
+    printf("Notas de R$ 50:  %d\n", n50);
+    printf("Notas de R$ 10:  %d\n", n10);
+    printf("Notas de R$ 5:   %d\n", n5);
+    printf("Notas de R$ 2:   %d\n", n2);
+    printf("Notas de R$ 1:   %d\n", n1);
+
     return 0;
 }
-      
-  
+
+// EXERCÍCIO 5
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+
+    float v0, angulo, alcance, tempo,rad, vx, vy, x, y, t;
+    
+    float g = 9.8;
+    float k = 0.5;
+    float pi = 3.14;
+
+    printf("Velocidade Inicial? ");
+    scanf("%f", &v0);
+
+    printf("Angulo? ");
+    scanf("%f", &angulo);
+
+
+    rad = angulo * (pi / 180);
+
+    vx = v0 * cos(rad);
+    vy = v0 * sin(rad);
+    t = 0;
+    y = 0;
+
+    while (y >= 0) {
+        t = t + 0.01;
+        x = (vx / k) * (1 - exp(-k * t));
+
+        y = (vy / k) * (1 - exp(-k * t))
+            - (g / k) * t
+            + (g / (k * k)) * (1 - exp(-k * t));
+    }
+
+    alcance = x;
+    tempo = t;
+
+    printf("\nAlcance Maximo: %.2f metros\n", alcance);
+    printf("Tempo de Voo: %.2f segundos\n", tempo);
+
+    return 0;
+}
+
+// EXERCÍCIO 6
+#include <stdio.h>
+
+float calcularINSS(float salario) { // cálculo do INSS
+
+    float valor;
+
+    if (salario <= 1412.00) {
+        valor = (7.5 / 100) * salario;
+    }
+    else if (salario <= 2666.68) {
+        valor = (9.0 / 100) * salario;
+    }
+    else if (salario <= 4000.03) {
+        valor = (12.0 / 100) * salario;
+    }
+    else {
+        valor = (14.0 / 100) * salario;
+    }
+
+    return valor;
+}
+
+int main(int argc, char *argv[]) {  // programa principal
+
+    float salario, desconto;
+
+    printf("Valor do salario bruto? ");
+    scanf("%f", &salario);
+
+    desconto = calcularINSS(salario);
+
+    printf("Desconto do INSS: %.2f\n", desconto);
+
+    return 0;
+}
+
+// EXERCÍCIO 7
+#include <stdio.h>
+
+float calcularIRPF(float irpf) { // cálculo do IRPF
+
+    float valor;
+
+    if (irpf <= 2259.20) {
+        valor = 0.00;
+    }
+    else if (irpf <= 2826.65) {
+        valor = (irpf * (7.5 / 100)) - 169.44;
+    }
+    else if (irpf <= 3751.05) {
+        valor = (irpf * (15.0 / 100)) - 381.44;
+    }
+    else if (irpf <= 4664.68) {
+        valor = (irpf * (22.5 / 100)) - 662.77;
+    }
+    else {
+        valor = (irpf * (27.5 / 100)) - 896.00;
+    }
+
+    return valor;
+}
+
+int main(int argc, char *argv[]) {  // programa principal
+
+    float salario, desconto;
+
+    printf("Valor do salário base? ");
+    scanf("%f", &salario);
+
+    desconto = calcularIRPF(salario);
+
+    printf("Desconto do IRPF: %.2f\n", desconto);
+
+    return 0;
+}
+
+// EXERCÍCIO 8
+#include <stdio.h>
+
+float calcularINSS(float salario) {
+
+    float valor;
+
+    if (salario <= 1412.00) {
+        valor = (7.5 / 100) * salario;
+    }
+    else if (salario <= 2666.68) {
+        valor = (9.0 / 100) * salario;
+    }
+    else if (salario <= 4000.03) {
+        valor = (12.0 / 100) * salario;
+    }
+    else {
+        valor = (14.0 / 100) * salario;
+    }
+
+    return valor;
+}
+
+float calcularIRPF(float irpf) {
+
+    float valor;
+
+    if (irpf <= 2259.20) {
+        valor = 0.00;
+    }
+    else if (irpf <= 2826.65) {
+        valor = (irpf * (7.5 / 100)) - 169.44;
+    }
+    else if (irpf <= 3751.05) {
+        valor = (irpf * (15.0 / 100)) - 381.44;
+    }
+    else if (irpf <= 4664.68) {
+        valor = (irpf * (22.5 / 100)) - 662.77;
+    }
+    else {
+        valor = (irpf * (27.5 / 100)) - 896.00;
+    }
+
+    return valor;
+}
+
+int main(int argc, char *argv[]) {
+
+    float valor_hora, horas, salario_bruto, inss, irpf, salario_liquido;
+
+    printf("Valor da hora trabalhada? ");
+    scanf("%f", &valor_hora);
+
+    printf("Quantidade de horas no mês? ");
+    scanf("%f", &horas);
+
+    salario_bruto = valor_hora * horas;
+
+    inss = calcularINSS(salario_bruto);
+    irpf = calcularIRPF(salario_bruto);
+
+    salario_liquido = salario_bruto - inss - irpf;
+
+    printf("\n========================================\n");
+    printf("          CONTRA-CHEQUE\n");
+    printf("========================================\n");
+    printf("Salário Bruto:       R$ %.2f\n", salario_bruto);
+    printf("Desconto INSS:       R$ %.2f\n", inss);
+    printf("Desconto IRPF:       R$ %.2f\n", irpf);
+    printf("----------------------------------------\n");
+    printf("Salário Líquido:     R$ %.2f\n", salario_liquido);
+    printf("========================================\n");
+
+    return 0;
+}
